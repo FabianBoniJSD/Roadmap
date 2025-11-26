b1fc7bc6e9efd4345e2edf4a43dd81630f413cc4
Fabian Boni <fabian.boni@jsd.bs.ch>
2025-11-26 08:53:13 +0100

    fix port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,13 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: "3000"
-      }
+      },
+      kill_timeout: 5000,
+      wait_ready: true,
+      listen_timeout: 10000,
+      autorestart: true,
+      max_restarts: 3,
+      min_uptime: "10s"
     }
   ]
 };
